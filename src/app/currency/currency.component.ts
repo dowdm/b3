847e5a6c54f16436e22d133a49119cea94d40f91
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-14 12:07:02 -0700

    add math function for calculating final amount

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -12,13 +12,16 @@ export class CurrencyComponent {
 
 
   exchanges: any[]=null;
-  rate: number = 0;
+  rate: string = "";
+  result: number = 0;
     constructor(private apiDataService: ApiDataService) { }
 
-    triggerExchangeRequest(source: string, destination: string) {
+    triggerExchangeRequest(source: string, destination: string, amount: string) {
       this.apiDataService.getCurrencyExchangeRate(source, destination).subscribe(response => {
           this.exchanges = response.json();
           this.rate = this.exchanges["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+          console.log(typeof(amount))
+          this.result = (parseFloat(amount) * parseFloat(this.rate));
       });
 
     }
