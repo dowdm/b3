b324cb2dafefddd200e2f3aa0dddc4141568f76b
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-14 13:43:55 -0700

    add crypto component

---
+++ src/app/crypto/crypto.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ApiDataService } from '../api-data.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-currency',
+  templateUrl: './currency.component.html',
+  styleUrls: ['./currency.component.css'],
+  providers: [ ApiDataService ]
+})
+export class CryptoComponent {
+
+
+  exchanges: any[]=null;
+  rate: string = "";
+  result: number = 0;
+    constructor(private apiDataService: ApiDataService) { }
+
+    triggerExchangeRequest(source: string, destination: string, amount: string) {
+      this.apiDataService.getCurrencyExchangeRate(source, destination).subscribe(response => {
+          this.exchanges = response.json();
+          this.rate = this.exchanges["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+          console.log(typeof(amount))
+          this.result = (parseFloat(amount) * parseFloat(this.rate));
+      });
+
+    }
+
+}
