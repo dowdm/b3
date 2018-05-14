b468e4b3045ddeffca9061e9c7a964557c68536b
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-14 08:40:23 -0700

    generate new project files and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule
