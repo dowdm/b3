5a2c9dc31d9826cc903c11e28317de1035e8ecb3
Matt Dowdney and semhar kidane <matt@>
2018-05-15 14:40:29 -0700

    add currency purchase input

--- src/app/api-data.service.ts
+++ src/app/api-data.service.ts
@@ -8,11 +8,15 @@ export class ApiDataService {
 
   constructor(private http: Http) { }
   getCurrencyExchangeRate(source: string, destination: string) {
-    // return this.http.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo`)
     return this.http.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${source}&to_currency=${destination}&apikey=${apikey}`)
   }
 
   getCryptoExchangeRate(symbol: string, market: string) {
     return this.http.get(`https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=${symbol}&market=${market}&apikey=${apikey}`)
   }
+
+  buyCurrencyExchangeRate(destination: string) {
+    return this.http.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=${destination}&apikey=${apikey}`)
+  }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { Account } from '../models/account.model';
 import { AccountsService } from '../accounts.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ApiDataService } from '../api-data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [AccountsService]
+  providers: [AccountsService, ApiDataService]
 })
 export class LoginComponent implements OnInit {
+  
 
+  exchanges: any[]=null;
+  rate: string = "";
+  result: number = 0;
 
-
-
-  constructor(private accountsService: AccountsService) { }
+  constructor(private accountsService: AccountsService, private apiDataService: ApiDataService) { }
 
   accounts: FirebaseListObservable<any[]>;
   assetsToDisplay: string[];
 
   ngOnInit() {
      this.accounts = this.accountsService.getAccounts();
-    //  this.assetsToDisplay = this.accounts[0].asset
+  }
+
+  triggerBuyRequest(destination: string, amount: string) {
+    this.apiDataService.buyCurrencyExchangeRate( destination).subscribe(response => {
+        this.exchanges = response.json();
+        this.rate = this.exchanges["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+
+        this.result = (parseFloat(amount) * parseFloat(this.rate));
+    });
+
   }
 }
